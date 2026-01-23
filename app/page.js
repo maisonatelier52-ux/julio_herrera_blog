a8dea8bd792c82e5e15875d14c48ca72568641c8
news structure <maisonatelier52@gmail.com>
2026-01-23 15:18:43 +0530

    chnage the image

--- app/page.js
+++ app/page.js
@@ -40,9 +40,10 @@ export default function Home() {
           <div className="lg:flex-1">
             <div className="w-full h-[300px] sm:h-[500px] lg:h-[500px]">
               <img
-                src="/images/julio-image15.png"
+                src="/images/julio-image15 (1).png"
                 alt="Julio Herrera Velutni"
                 className="w-full h-full object-cover rounded-lg lg:rounded-none"
+                
               />
             </div>
           </div>
