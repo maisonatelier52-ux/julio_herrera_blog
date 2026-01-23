fb0cff0d9701a99131aad5ed793df241dd4c03f5
news structure <maisonatelier52@gmail.com>
2026-01-23 16:17:36 +0530

    chnage the height of image

--- app/page.js
+++ app/page.js
@@ -113,7 +113,7 @@ export default function Home() {
 
       {/* Column 2: Image */}
       <div className="lg:flex-1">
-        <div className="w-full min-h-[400px] sm:h-[500px] lg:h-[500px]">
+        <div className="w-full min-h-[330px] sm:h-[500px] lg:h-[500px]">
           <img
             src="/images/julio-image15 (1) (1).png"
             alt="Julio Herrera Velutni"
