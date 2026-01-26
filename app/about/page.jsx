09d092f01998b0a1d5ad783012800f3b9fe84e57
news structure <maisonatelier52@gmail.com>
2026-01-26 14:56:07 +0530

    remove a word from about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -40,7 +40,7 @@ export default function About() {
 
             <div className="mt-8 space-y-6 max-w-xl">
               <p className="text-base leading-relaxed font-libre-baskerville">
-                Julio Herrera Velutini is an Italian–Venezuelan banker, entrepreneur, and global financial leader. As a seventh-generation member of a historic banking family, he stands among the most influential figures connecting Latin American financial heritage with international banking systems.
+                Julio Herrera Velutini is an Italian– banker, entrepreneur, and global financial leader. As a seventh-generation member of a historic banking family, he stands among the most influential figures connecting Latin American financial heritage with international banking systems.
               </p>
               
               <p className="text-base leading-relaxed font-libre-baskerville">
@@ -102,7 +102,7 @@ export default function About() {
 
             <div className="mt-6 space-y-4 text-left px-4">
               <p className="text-xs sm:text-base leading-relaxed font-libre-baskerville">
-                Julio Herrera Velutini is an Italian–Venezuelan banker, entrepreneur, and global financial leader. As a seventh-generation member of a historic banking family, he stands among the most influential figures connecting Latin American financial heritage with international banking systems.
+                Julio Herrera Velutini is an Italian– banker, entrepreneur, and global financial leader. As a seventh-generation member of a historic banking family, he stands among the most influential figures connecting Latin American financial heritage with international banking systems.
               </p>
               
               <p className="text-xs sm:text-base leading-relaxed font-libre-baskerville">
